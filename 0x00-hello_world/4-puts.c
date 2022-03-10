a595ab86671201ada0c8536a6f07e00a24579101
MsLynette <lynettekatel@gmail.com>
2022-03-10 14:20:24 -0800

    My puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include <stdio.h>                                                                                         
-/*                                                                                                                                           
+#include <stdio.h>
+
+/*
  *
- *main - entry point                                                                                                                        
+ *main - entry point
  *
  *Return: Always 0 (Success)
  */
-int main(void) 
-{ 
-	 puts("\"Programming is like building a multilingual puzzle");                                                                                                  return 0;
+int main(void)
+{
+	 puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
 }
